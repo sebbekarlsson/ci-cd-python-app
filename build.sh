86bdec5d9b0afdd8b3662fe1bb9daf8d225f58ab
ianertson <sebbekarlsson97@gmail.com>
2021-03-29 15:48:02 +0200

    adds comments and changes structure of files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# this is our "build"
+tar czf app.tar.gz application/*
--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
-tar czf app.tar.gz application/*
+# create build
+./build.sh
 
-
+# install dependencies required for our application to run
 ansible-playbook -i ansible/inventory.yml ansible/playbooks/install.yml
+
+# copy over application files to webserver
 ansible-playbook -i ansible/inventory.yml ansible/playbooks/copy_files.yml
+
+# setup mysql, create users, datbase etc
 ansible-playbook -i ansible/inventory.yml ansible/playbooks/mysql.yml
+
+# finally, start application
 ansible-playbook -i ansible/inventory.yml ansible/playbooks/start_app.yml
